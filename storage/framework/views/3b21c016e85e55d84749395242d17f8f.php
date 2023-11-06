d3bd79ce97644107e7d1ed0405d4f3ec379eaebd
gjmarrero <143582283+gjmarrero@users.noreply.github.com>
2023-11-06 16:23:15 +0800

    Add folder

---
+++ storage/framework/views/3b21c016e85e55d84749395242d17f8f.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Provincial Legal Office</title>
+
+    <?php echo app('Illuminate\Foundation\Vite')(['resources/css/app.css', 'resources/js/app.js']); ?>
+</head>
+
+<body class="hold-transition login-page">
+    <div id="login">
+            <Login />
+    </div>
+</body>
+
+</html><?php /**PATH C:\xampp\htdocs\legal-office\resources\views/auth/login.blade.php ENDPATH**/ ?>
