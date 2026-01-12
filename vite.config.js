afb954ca4a05bc1a21a0c0017bace2524d04f1d7
gjmarrero <143582283+gjmarrero@users.noreply.github.com>
2026-01-12 11:22:13 +0800

    Updage gitignore

--- vite.config.js
+++ vite.config.js
@@ -13,14 +13,14 @@ export default defineConfig({
     //     },
     // },
     server: {
-        host: 'localhost', // this lets Vite accept external connections
-        port: 5173,
+        // host: 'localhost',
+        // port: 5173,
         cors: true,
-        // hmr: {
-        //     host: '192.168.6.109', 
-        //     protocol: 'ws',
-        //     port: 5173,
-        // },
+        hmr: {
+            host: '192.168.6.109', 
+            protocol: 'ws',
+            port: 5173,
+        },
     },
     esbuild: {
         supported: {
